bf7232612ca1ced0ec93b5c40d8e0da53e5f0f41
Pavel Filippenko <rubikpf2002@mail.ru>
2023-09-02 10:20:06 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,35 @@
+//* Compilation: g++ -fopenmp main.cpp 
+
+#include <iostream>
+#include <omp.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "[-] Usage %s n -- the number of threads\n", argv[0]);
+        return -1;
+    }
+
+    uint32_t threads_num = atoi(argv[1]);
+
+    std::cout << threads_num << std::endl;
+
+    // set the number of threads
+    omp_set_num_threads(threads_num);
+    // start the parallel section
+    #pragma omp parallel
+    {
+        uint32_t thread_num = omp_get_thread_num();
+        // if we don't do it, lot of threads access to the stdout at the same time,
+        // and we have race condition with output resource.
+        // If you're really interested in it, you can just comment this part of code,
+        // and try to compile and run the program
+        sleep(thread_num);
+
+        std::cout << "Hello, world! Thread number [" << thread_num << "]" << std::endl;
+        // barrier synchronisation
+        #pragma omp barrier 
+    }
+
+    return 0;
+}
